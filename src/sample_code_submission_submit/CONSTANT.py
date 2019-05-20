6ae6525083081918de9c504341d6caa9196330a0
Xi-Jun Lee <xijun.lee@hotmail.com>
2019-05-20 23:43:43 +0800

    modified

--- src/sample_code_submission_submit/CONSTANT.py
+++ src/sample_code_submission_submit/CONSTANT.py
@@ -23,11 +23,11 @@ LABEL = "label"
 # 1. The larger the HASH_MAX, the less information from other records with identical hash value can be used.
 # 2. The larger the WINDOW_SIZE, the more temporal information can be used.
 HASH_MAX = 100
-WINDOW_SIZE = 10
+WINDOW_SIZE = 20
 
 
 # the VARIANCE RAITO is used in PCA
-VARIANCE_RATIO = 0.90
+VARIANCE_RATIO = 0.95
 
 REDUCTION_SWITCH = True
 
