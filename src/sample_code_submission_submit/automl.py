5a586ba7b9e3488bc7357b3787e1231c6f3dc82e
Zihan Zhang <dreamlh@outlook.com>
2019-06-25 16:52:02 +0800

    fix a bug

--- src/sample_code_submission_submit/automl.py
+++ src/sample_code_submission_submit/automl.py
@@ -204,6 +204,7 @@ def hyperopt_lightgbm(X: pd.DataFrame, y: pd.Series, params: Dict, config: Confi
     def objective(hyperparams):
         model_li = []
         if STOCHASTIC_CV:
+            X_, y_ = data_sample(X, y, TRAIN_DATA_SIZE, random_state=SEED)
             X_train, X_val, y_train, y_val = data_split(X_, y_, test_size=0.2, random_state=SEED)
             train_data = lgb.Dataset(X_train, label=y_train, free_raw_data=True)
             valid_data = lgb.Dataset(X_val, label=y_val, free_raw_data=True)
